7495d7105c112d898405d261e740edb01603c68a
Teo Ene <tdene@sakuya.ecen.okstate.edu>
2020-10-14 17:11:25 -0500

    Fixed most bugs. scale_congestion needs to be fixed to actually account for tracks.

--- model/design.py
+++ model/design.py
@@ -1,29 +1,28 @@
 #!/usr/bin/env python3
 
-import sys
-sys.path.append("..")
+import sys,math
 
 from model import params, net
 
 def scale_congestion(x):
-    return math.exp(x)-1
+    return (math.exp(x)-1)
 
 # Basically a 2-dimensional matrix
 class design:
     
     # Each square is [{n -> [v]},{L -> c}]
-    grid=[[]]
+##    grid=[[]]
     # Stored just because old code convention
     # calls on the coder to store list sizes
-    x_dim=0
-    y_dim=0
+##    xdim=0
+##    ydim=0
 
     # Dictionary of net objects. {name -> net}
-    nets={}
+##    nets={}
 
-    # Only one net "active" at a time
+    # Only one vertex "active" at a time
     # Switch action must be taken to switch
-    active_net=-1
+##    active=[0,0]
 
     # DEF: [(cell_name,cell_type,llx,lly)]
     # stdcells: [(cell_type,(out_pins),(in_pins)]
@@ -31,14 +30,20 @@ class design:
     # netlist: [(cell_name,cell_type,{pin_name -> net_name})]
     def __init__(self,DEF,stdcells,nets,netlist):
 
+        self.grid=[[]]
+        self.xdim=0
+        self.ydim=0
+        self.nets={}
+        self.active=[0,0]
+
         # Define grid itself
         # Each square is [x,y,{n -> [v]},{L -> c}]
 
         # Grab the dimensions of the grid and initialize it
         round_up = lambda x,y: int(x/y)+(x%y > 0)
-        self.x_dim=round_up(params.GX,params.GSQ)
-        self.y_dim=round_up(params.GY,params.GSQ)
-        self.grid=[[[{},{}] for j in range(self.y_dim)] for i in range(self.x_dim)]
+        self.xdim=round_up(params.GX,params.GSQ)
+        self.ydim=round_up(params.GY,params.GSQ)
+        self.grid=[[[{},{}] for j in range(self.ydim)] for i in range(self.xdim)]
 
         # Parse the inputs
         std_cell_dic = {a:{'out_pins':b,'in_pins':c} for a,b,c in stdcells}
@@ -72,8 +77,7 @@ class design:
         # Iterate through net_dic
         for name,n in net_dic.items():
             # Create net objects
-            net_obj=net.net(n)
-            self.nets[name]=net_obj
+            self.nets[name]=net.net(n)
             # Populate the grid object's net dict
             for i in range(len(n)):
                 x = n[i]
@@ -81,16 +85,19 @@ class design:
                 ret.append(i)
                 self.grid[x[0]][x[1]][0][name]=ret
         # Since we're init-ing, congestion only depends on vertices
-        for x in range(self.x_dim):
-            for y in range(self.y_dim):
+        for x in range(self.xdim):
+            for y in range(self.ydim):
                 # Initial pins are on layers 0 and 1
                 self.grid[x][y][-1][0]=len(self.grid[x][y][0])
                 self.grid[x][y][-1][1]=self.grid[x][y][-1][0]
 
         # Choose random starting active net for now
         active_net = list(self.nets.keys())[0]
+        n_temp = self.nets[active_net]
+        v_temp = 0
+        self.active = [active_net,v_temp]
 
-    def get_state():
+    def get_state(self):
         # State will be 3-dimensional matrix
         # One dimension is X
         # One dimension is Y
@@ -102,47 +109,83 @@ class design:
         # this is a neural network. That information can help it
         # figure out how switching works, since it actually sees
         # the vertices it can switch to.
-        state=[[]*self.y_dim]*self.x_dim
-        for a in self.xdim:
-            for b in self.ydim:
+        state=[[0 for j in range(self.ydim)] for i in range(self.xdim)]
+        for a in range(self.xdim):
+            for b in range(self.ydim):
                 el = self.grid[a][b]
                 ndic = el[0]
                 ndic_parsed = {}
                 el_parsed = []
                 # Turn the 1st element of a grid square, {n -> [v]}
                 # Into a count of vertices per layer
-                for v in ndic.values():
-                    for l in v[-1]:
-                        ndic_parsed[l]=ndic_parsed.get(l,0)+1
-                for i in range(params.MMi+1):
-                    el_parsed.append(el[-1][i])
-                for i in range(params.MMi+1):
-                    el_parsed.append(ndic_parsed[i])
+                for n,v in ndic.items():
+                    net_obj = self.nets[n]
+                    vertices = [net_obj.v[x] for x in v]
+                    for l in [x[-1] for x in vertices]:
+                        for k in l:
+                            ndic_parsed[k]=ndic_parsed.get(k,0)+1
+                for i in range(params.MM+1):
+                    el_parsed.append(el[-1].get(i,0))
+                for i in range(params.MM+1):
+                    el_parsed.append(ndic_parsed.get(i,0))
                 state[a][b]=el_parsed
         return state
 
-    def do_action(X):
-        if X in ['N','S','E','W','up','down']:
-            self.active_net.move(X)
-            # Need to change grid object
-            return
+    def do_action(self,X):
+        # Get names of active net and vertex
+        [active_n,active_v] = self.active
+        # Get actual net object
+        net_obj = self.nets[active_n]
+        # Store name and contents of old vertex
+        old_n,old_v = (active_v,net_obj.v[active_v])
+
+        # Act only if action is valid
+        # Otherwise nothing happens
+        # We don't stop the NN from choosing invalid actions?
+        # We just ignore and do nothing?
+        # I guess the NN can just shout into the void if it feels the need
+        # I know routing can be pretty frustrating.
+        if X in net_obj.valid_actions(active_v):
+            # Move, and store name and contents of new vertex
+            new_n,new_v=net_obj.move(active_v,X)
+
+            # Congestion ALWAYS increases
+            # in new location
+            # ONLY on active Layer
+            self.grid[new_v[0]][new_v[1]][-1][new_v[-2]]+=1
+
+            # Update new position with vertex, if vertex moved
+            tmp=self.grid[new_v[0]][new_v[1]][0].get(active_n,[])
+            if new_n not in tmp:
+                tmp.append(new_n)
+            self.grid[new_v[0]][new_v[1]][0][active_n]=tmp
+            del tmp
+
+            # If the vertex moved, modify the old vertex
+            if len(old_v[-1])==1:
+                tmp=self.grid[old_v[0]][old_v[1]][0][active_n]
+                tmp.remove(old_n)
+                self.grid[old_v[0]][old_v[1]][0][active_n]=tmp
+
+            # Switch active to new vertex
+            self.active=[active_n,new_n]
         # Need to implement switch
         pass
 
-    def global_loss(state):
+    def global_loss(self,state):
         ret1=0
         ret2=0
-        for n in self.nets:
-            ret+=n.loss()
-        for (x,y) in zip(range(self.xdim),range(self.ydim)):
-            for L in range(params.MM):
-                ret1+=scale_congestion(state[x][y][L])
+        for n in self.nets.values():
+            ret1+=n.loss()
+        for x in range(self.xdim):
+            for y in range(self.ydim):
+                for L in range(params.MM):
+                    ret2+=scale_congestion(state[x][y][L])
         # Average it out. No need, but it does lower the number.
         # Maybe normalizing is good because nets vs congestion.
         ret1 = ret1/len(self.nets)
-        ret2 = ret2/self.xdim
-        ret2 = ret2/self.ydim
-
+#        ret2 = ret2/self.xdim
+#        ret2 = ret2/self.ydim
         return ret1+ret2
 
 
--- unit_tests/grid_test.py
+++ unit_tests/grid_test.py
@@ -19,13 +19,59 @@ DEF.append(["cell3","INV",3,3])
 
 nets.append("net1")
 nets.append("net2")
+nets.append("net3")
 
-netlist.append(["cell1","INV",{"Y":"net1"}])
-netlist.append(["cell3","INV",{"A":"net2"}])
-netlist.append(["cell2","AND",{"A":"net1","Y":"net2"}])
+netlist.append(["cell1","INV",{"A":"net3","Y":"net1"}])
+netlist.append(["cell3","INV",{"A":"net2","Y":"net3"}])
+netlist.append(["cell2","AND",{"A":"net1","B":"net3","Y":"net2"}])
 
 des = design.design(DEF,stdcells,nets,netlist)
+
+for n in des.nets.values():
+    print(n)
 
 print(des.grid[0][0])
 print(des.grid[5][5])
 print(des.grid[3][3])
+print(des.active)
+
+state=des.get_state()
+print(des.nets["net1"].loss())
+print(des.nets["net2"].loss())
+print(des.nets["net3"].loss())
+print(des.global_loss(state))
+
+print(des.nets["net1"].loss())
+print(des.global_loss(state))
+print(des.nets["net1"])
+
+des.do_action("N")
+print(des.nets["net1"].loss())
+print(des.global_loss(state))
+print(des.nets["net1"])
+des.do_action("N")
+print(des.nets["net1"].loss())
+print(des.global_loss(state))
+print(des.nets["net1"])
+des.do_action("N")
+print(des.nets["net1"].loss())
+print(des.global_loss(state))
+print(des.nets["net1"])
+des.do_action("E")
+print(des.nets["net1"].loss())
+print(des.global_loss(state))
+print(des.nets["net1"])
+des.do_action("E")
+print(des.nets["net1"].loss())
+print(des.global_loss(state))
+print(des.nets["net1"])
+des.do_action("N")
+print(des.nets["net1"].loss())
+print(des.global_loss(state))
+print(des.nets["net1"])
+
+#for a in des.nets.values():
+#    print(a.loss())
+
+state=des.get_state()
+print(des.global_loss(state))
